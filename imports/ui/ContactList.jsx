1e9a9cdc7aad765d2cafa421f37b1e66a78f0a62
Nica An <116259616+VeronicaAn99@users.noreply.github.com>
2023-12-17 20:52:22 +0200

    Using pubications, useFind and useSubscribe

--- imports/ui/ContactList.jsx
+++ imports/ui/ContactList.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { memo } from "react";
 import { ContactCollection } from "../api/ContactCollection";
-import { useTracker } from "meteor/react-meteor-data";
+import { useSubscribe, useFind } from "meteor/react-meteor-data";
 
 export const ContactList = () => {
-  const contacts = useTracker(() => {
-    Meteor.subscribe("contacts");
+  const isLoading = useSubscribe("allContacts");
+  const contacts = useFind(() => {
     return ContactCollection.find(
       {},
       {
         sort: { createdAt: -1 },
       },
-    ).fetch();
+    );
   });
+
+  if (isLoading()) {
+    return (
+      <div className="px-6">
+        <div className="mt-10">
+          <h3 className="text-xs font-semibold text-gray-500 uppercase tracking-wide">Loading...</h3>
+        </div>
+      </div>
+    ) 
+  }
 
   const removeContact = (e, _id) => {
     e.preventDefault();
@@ -19,6 +29,39 @@ export const ContactList = () => {
       contactId: _id,
     });
   };
+
+  const ContactItem = memo(({ person }) => {
+    return (
+      <li className="py-4 flex items-center justify-between space-x-3">
+        <div className="min-w-0 flex-1 flex items-center space-x-3">
+          <div className="flex-shrink-0">
+            <img
+              className="h-10 w-10 rounded-full"
+              src={person.imageUrl}
+              alt=""
+            />
+          </div>
+          <div className="min-w-0 flex-1">
+            <p className="text-sm font-medium text-gray-900 truncate">
+              {person.name}
+            </p>
+            <p className="text-sm font-medium text-gray-500 truncate">
+              {person.email}
+            </p>
+          </div>
+          <div>
+            <a
+              href="#"
+              onClick={(e) => removeContact(e, person._id)}
+              className="inline-flex items-center shadow-sm px-0.5 border border-grey-300 text-sm font-medium rounded-full text-gray-900"
+            >
+              Remove
+            </a>
+          </div>
+        </div>
+      </li>
+    );
+  });
 
   return (
     <div className="px-6">
@@ -30,38 +73,8 @@ export const ContactList = () => {
           role="list"
           className="mt-4 border-t border-b border-gray-200 divide-y divide-gray-200"
         >
-          {contacts.map((person, personIdx) => (
-            <li
-              key={personIdx}
-              className="py-4 flex items-center justify-between space-x-3"
-            >
-              <div className="min-w-0 flex-1 flex items-center space-x-3">
-                <div className="flex-shrink-0">
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src={person.imageUrl}
-                    alt=""
-                  />
-                </div>
-                <div className="min-w-0 flex-1">
-                  <p className="text-sm font-medium text-gray-900 truncate">
-                    {person.name}
-                  </p>
-                  <p className="text-sm font-medium text-gray-500 truncate">
-                    {person.email}
-                  </p>
-                </div>
-                <div>
-                  <a
-                    href="#"
-                    onClick={(e) => removeContact(e, person._id)}
-                    className="inline-flex items-center shadow-sm px-0.5 border border-grey-300 text-sm font-medium rounded-full text-gray-900"
-                  >
-                    Remove
-                  </a>
-                </div>
-              </div>
-            </li>
+          {contacts.map((person) => (
+            <ContactItem key={person._id} person={person} />
           ))}
         </ul>
       </div>
--- server/main.js
+++ server/main.js
@@ -2,7 +2,8 @@ import { Meteor } from 'meteor/meteor';
 import "../imports/api/ContactCollection";
 import "../imports/api/ContactsMethod";
 import {ContactCollection} from "../imports/api/ContactCollection";
+import "../imports/api/ContactsPublication";
 
 Meteor.startup(async () => {
-  Meteor.publish('contacts', function () { return ContactCollection.find()});
+//   Meteor.publish('contacts', function () { return ContactCollection.find()});
 });
